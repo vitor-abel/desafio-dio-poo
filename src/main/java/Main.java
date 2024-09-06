47d6f523d527661871575cdf2e523d26a1a0ddd9
Abel <abelabelao@hotmail.com.br>
2024-09-06 10:06:11 -0300

    Mentoria e Curso

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import desafio.dominio.Curso;
+import desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Javascript");
+        curso2.setDescricao("Descrição Curso Java");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição Mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+    }
+}
